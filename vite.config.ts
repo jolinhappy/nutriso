82f5152c96b851a707b69ed3e41668f617b1a936
Phoebe <jolinhappy1990@hotmail.com>
2024-06-20 23:11:26 +0800

    feat: add components

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 import checker from 'vite-plugin-checker';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +16,11 @@ export default defineConfig({
       },
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
   resolve: {
     alias: {
       SRC: fileURLToPath(new URL('./src', import.meta.url)),
