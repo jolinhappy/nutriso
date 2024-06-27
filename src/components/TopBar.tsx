f4a14825efd07b7e1739ddaecb20a51e09f87224
Phoebe <jolinhappy1990@hotmail.com>
2024-06-27 23:22:10 +0800

    feat: add change language function

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Lang } from 'SRC/constants/common';
+import { useTranslation } from 'react-i18next';
+import { Dropdown, IDropdownItem } from '@pinkymini/nutriso-components-new';
 
 const TopBar = () => {
+  const { i18n } = useTranslation();
+  const [currentLang, setCurrentLang] = useState(Lang.TW);
+  const dropdownList: IDropdownItem[] = [
+    {
+      value: Lang.TW,
+      optionText: '繁體中文',
+    },
+    {
+      value: Lang.EN,
+      optionText: 'English',
+    },
+    {
+      value: Lang.KR,
+      optionText: '한국어',
+    },
+  ];
+
+  useEffect(() => {
+    i18n.changeLanguage(currentLang);
+  }, [currentLang, i18n]);
+
   return (
-    <div className="drop-shadow-lg bg-white px-4 py-3 flex items-center h-[75px]">
+    <div className="drop-shadow-lg bg-white px-10 py-3 flex items-center h-[75px] justify-between">
       <Link className="" to={{ pathname: '/' }}>
         <img className="h-[50px]" src="SRC/assets/logo.png" alt="" />
       </Link>
+      <Dropdown dropdownList={dropdownList} value={currentLang} onValueChange={setCurrentLang}>
+        {dropdownList.find((item) => item.value === currentLang)?.optionText}
+      </Dropdown>
     </div>
   );
 };
